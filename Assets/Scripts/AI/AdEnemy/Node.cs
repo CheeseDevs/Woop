c2f2a404fe70f756f797be474f6e56c947400a97
drinkingWater <61174663+DrinkingWater64@users.noreply.github.com>
2023-01-05 23:36:18 +0600

    added sequencer and selector

--- Assets/Scripts/AI/AdEnemy/Node.cs
+++ Assets/Scripts/AI/AdEnemy/Node.cs
@@ -107,6 +107,78 @@ namespace BT
 
         protected abstract Node SetupTree();
     }
+
+    public class Sequence: Node
+    {
+        public Sequence(): base()
+        {
+
+        }
+        public Sequence(List<Node> children): base(children)
+        {
+
+        }
+
+        public override NodeState Evaluate()
+        {
+            bool anyChildIsRunning = false;
+            foreach (Node node in children)
+            {
+                switch (node.Evaluate())
+                {
+                    case NodeState.RUNNING:
+                        anyChildIsRunning = true;
+                        continue;
+                    case NodeState.SUCCESS:
+                        continue;
+                    case NodeState.FAILURE:
+                        state = NodeState.FAILURE;
+                        return state;
+                    default:
+                        state = NodeState.SUCCESS;
+                        return state;
+                }
+            }
+            state = anyChildIsRunning ? NodeState.RUNNING : NodeState.SUCCESS;
+            return state;
+        }
+    }
+
+    public class Selector: Node
+    {
+        public Selector() : base()
+        {
+
+        }
+        public Selector(List<Node> children) : base(children)
+        {
+
+        }
+
+        public override NodeState Evaluate()
+        {
+            foreach (Node node in children)
+            {
+                switch (node.Evaluate())
+                {
+                    case NodeState.RUNNING:
+                        state = NodeState.RUNNING;
+                        return state;
+                    case NodeState.SUCCESS:
+                        state = NodeState.SUCCESS;
+                        return state;
+                    case NodeState.FAILURE:
+                        continue;
+                    default:
+                        continue;
+                }
+            }
+            state = NodeState.FAILURE;
+            return state;
+        }
+    }
 }
+
+
 
 
