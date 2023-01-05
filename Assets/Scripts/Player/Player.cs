d7aa50da19c454d3eb778ba93148bc49c969d848
Chaositer <zahin.shabab.abir@gmail.com>
2023-01-06 05:19:05 +0600

    minor tweaks

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -16,7 +16,7 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Backspace))
         {
             TakeDamage(20);    
         }
