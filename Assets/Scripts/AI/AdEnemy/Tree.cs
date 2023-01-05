a8e8329e66d8ed4cf73c99189d4c02d922ee4efa
drinkingWater <61174663+DrinkingWater64@users.noreply.github.com>
2023-01-05 23:40:55 +0600

    cleaned package

---
+++ Assets/Scripts/AI/AdEnemy/Tree.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace BT
+{
+    public abstract class Tree: MonoBehaviour
+    {
+        private Node _root = null;
+
+        protected void Start()
+        {
+           
+        }
+
+        private void Update()
+        {
+            if (_root != null)
+            {
+                _root.Evaluate();
+            }
+        }
+
+        protected abstract Node SetupTree();
+    }
+}
+
+
+
+
