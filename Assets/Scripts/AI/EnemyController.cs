44b00200dbbfcfa6ec5e0c661755b3b50845c021
drinkingWater <61174663+DrinkingWater64@users.noreply.github.com>
2023-01-07 02:05:20 +0600

    enemy shoots bullet

--- Assets/Scripts/AI/EnemyController.cs
+++ Assets/Scripts/AI/EnemyController.cs
@@ -6,10 +6,26 @@ public class EnemyController : MonoBehaviour, IDamagable
 {
     [SerializeField]
     private float _health;
+    [SerializeField]
+    private bool _shouldShoot;
+    [SerializeField]
+    private float _fireRate;
+    [SerializeField]
+    private float _shotCounter;
+    [SerializeField]
+    private GameObject _bullet;
+    [SerializeField]
+    public Transform _firePoint;
+
+
+
 
     public float playerRange = 10f;
     public float speed;
     public Rigidbody2D rb;
+
+
+
 
     public void DealDamage(float damage)
     {
@@ -45,6 +61,20 @@ public class EnemyController : MonoBehaviour, IDamagable
         {
             Vector3 playerDir = PlayerMovement.instance.transform.position - transform.position;
             rb.velocity = playerDir * speed;
+
+            if (_shouldShoot)
+            {
+                _shotCounter -= Time.deltaTime;
+                if (_shotCounter <= 0)  
+                {
+                    Instantiate(_bullet, _firePoint.position, _firePoint.rotation);
+                    _shotCounter = _fireRate;
+                }
+            }
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
         }
     }
 }
