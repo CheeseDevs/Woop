50b06885873ce7085c927c0a4bd185cfaa831ff7
drinkingWater <61174663+DrinkingWater64@users.noreply.github.com>
2023-01-08 03:50:03 +0600

    Added animation to mainmenu

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -6,24 +6,46 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public Animator gunAnim;
-   public void Play()
-   {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        GunAnim();
-   }
+    [SerializeField] GameObject _Instruction;
+    [SerializeField] GameObject _MainMenu;
+    public void Play()
+    {
+        StartCoroutine(PlayGame());
+    }
 
     public void GunAnim()
     {
+        Debug.Log("Anim played");
         gunAnim.SetTrigger("Shoot");
     }
 
     public void Instruction()
     {
-        GunAnim();
+        StartCoroutine(HowToPlay());
     }
-   public void Exit()
-   {
-        Application.Quit();
+    public void Exit()
+    {
+        StartCoroutine(ExitGame());
+    }
+
+    public IEnumerator HowToPlay()
+    {
         GunAnim();
-   }
+        yield return new WaitForSeconds(.2f);
+        Debug.Log("How to");
+        _Instruction.SetActive(true);
+        _MainMenu.SetActive(false);
+    }
+    public IEnumerator PlayGame()
+    {
+        GunAnim();
+        yield return new WaitForSeconds(.2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public IEnumerator ExitGame()
+    {
+        GunAnim();
+        yield return new WaitForSeconds(.2f);
+        Application.Quit();
+    }
 }
