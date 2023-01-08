b6a795f79b050077705dc242248f72794e480361
drinkingWater <61174663+DrinkingWater64@users.noreply.github.com>
2023-01-08 09:27:13 +0600

    Fixed ammo

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -79,6 +79,7 @@ public class PlayerMovement : MonoBehaviour, IDamagable
                         if (hit.transform.CompareTag("Enemy"))
                         {
                             hit.transform.parent.GetComponent<IDamagable>().TakeDamage(30);
+                            hud.GetComponent<CanvasManager>().UpdateAmmo(currentAmmo);
                         }
                     }
                     else
